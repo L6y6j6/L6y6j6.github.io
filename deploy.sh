aa3b9da755222032a79c6ed543430b11e419b02a
Yanjin Li <yanjinli502@gmail.com>
2024-08-04 18:02:02 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ mkdocs build
 temp_dir=$(mktemp -d)
 
 # 克隆仓库的 gh-pages 分支到临时目录
-git clone -b gh-pages --single-branch https://github.com/l6y6j6/l6y6j6.github.io.git $temp_dir
+git clone -b gh-pages --single-branch https://github.com/L6y6j6/L6y6j6.github.io.git $temp_dir
 
 # 创建目标目录（如果不存在）
 mkdir -p $temp_dir/mydocs
@@ -29,3 +29,5 @@ git push origin gh-pages
 
 # 清理临时目录
 rm -rf $temp_dir
+
+echo "已清除 cache！"
